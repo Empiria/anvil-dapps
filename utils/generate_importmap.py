4d3979c3663e95a910c2c5ce256db6d79eb61fd9
Owen Campbell <owen.campbell@empiria.co.uk>
2024-08-14 14:22:54 +0100

    📝 Add a docstring to the generation script

--- utils/generate_importmap.py
+++ utils/generate_importmap.py
@@ -3,6 +3,16 @@
 # Copyright (c) 2024 Empiria Ltd
 #
 # This software is published at https://github.com/empiria/anvil-dapps
+"""Generate an import map and inject it into anvil.yaml
+
+To run this, you will need to install the jspm CLI tool: https://jspm.org/getting-started
+
+The resulting import map will be injected into anvil.yaml under the key `native_deps.import_map`.
+
+That's editable from within the IDE, but you have to navigate to it manually. Go to
+native libraries in the usual way but then, in the URL, replace 'native-libraries' with
+'native-deps'.
+"""
 
 import subprocess
 import io
